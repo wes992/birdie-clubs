e98fe1b4628d747c0d337ad65a606f94e21e21e5
Wes Hampton <wes.hampton@infiniteblue.com>
2024-08-13 10:08:12 -0400

    remove config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmL01Az-SbKyXpdQjGYmIYMUfJuLbmWMk",
-  authDomain: "birdie-clubs.firebaseapp.com",
-  databaseURL: "https://birdie-clubs-default-rtdb.firebaseio.com",
-  projectId: "birdie-clubs",
-  storageBucket: "birdie-clubs.appspot.com",
-  messagingSenderId: "615735088457",
-  appId: "1:615735088457:web:7d47b0594a72da0ea17698",
-  measurementId: "G-L4RPEVVVTZ",
-};
+import { firebaseConfig } from "./config";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
